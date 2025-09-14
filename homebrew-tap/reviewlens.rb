568bf664b46444d30bad3073354350fd88004abf
Benediktus Satriya <93780305+benebobaa@users.noreply.github.com>
2025-09-14 13:46:35 +0700

    chore(homebrew): update checksums for 1.0.0

--- homebrew-tap/reviewlens.rb
+++ homebrew-tap/reviewlens.rb
@@ -6,20 +6,20 @@ class ReviewLens < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Review-LensAi/reviewlens/releases/download/v\#{version}/reviewlens-aarch64-apple-darwin.tar.gz"
-      sha256 "f45f78b42491fa28a6d41a04ad488d75fc616a958182cb12794977152b035ac3"
+      sha256 "984c1d422681008b4a7d7ba7a05519cf6542b29d77b5c009836c186838d710db"
     else
       url "https://github.com/Review-LensAi/reviewlens/releases/download/v\#{version}/reviewlens-x86_64-apple-darwin.tar.gz"
-      sha256 "f45f78b42491fa28a6d41a04ad488d75fc616a958182cb12794977152b035ac3"
+      sha256 "4272c939af27ec09cd87cbe78cedded84fc133f84bd656e3fe2827ce99933bfc"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/Review-LensAi/reviewlens/releases/download/v\#{version}/reviewlens-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "f45f78b42491fa28a6d41a04ad488d75fc616a958182cb12794977152b035ac3"
+      sha256 "c845d5bcfeb8e45afeb3ffeb061dcdebff296ba9f73b3d0661726afc49afc41f"
     else
       url "https://github.com/Review-LensAi/reviewlens/releases/download/v\#{version}/reviewlens-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "f45f78b42491fa28a6d41a04ad488d75fc616a958182cb12794977152b035ac3"
+      sha256 "ae17f91b06e660145133de6ba806e2c8194835bc8bfb9e0030adcae57870d115"
     end
   end
 
